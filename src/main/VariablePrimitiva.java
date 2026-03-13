0f6a4f453966713cfdd7ebfc3c41ccad136fc124
Sara Ruiz <svruiz@uamv.edu.ni>
2026-03-13 15:33:50 -0600

    Variables

---
+++ src/main/VariablePrimitiva.java
@@ -0,0 +1,16 @@
+package main;
+
+public class VariablePrimitiva {
+    public static void main(String[] args) {
+        int numero=10;
+        System.out.println("El numero es: "+ numero);
+        float capital=1999999.99f;
+        double decimal=1999999.99;
+        System.out.println("EL capital es: "+capital);
+        System.out.println("EL capital es: "+decimal);
+        boolean estado= true;
+        System.out.println("El estado es: "+estado);
+        String nombre="Sara";
+    }
+
+}
---
+++ src/main/VariableReferencia.java
@@ -0,0 +1,9 @@
+package main;
+
+import java.util.Scanner;
+
+public class VariableReferencia {
+    public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
+    }
+}
