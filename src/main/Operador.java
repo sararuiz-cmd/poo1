845afbde04a4990f635ee7d311b5b2c546510d95
Sara Ruiz <svruiz@uamv.edu.ni>
2026-03-13 16:10:48 -0600

    Operadores

---
+++ src/main/Operador.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Operador {
+    public static void main(String[] args) {
+        int num1,num2;
+        num1=3;
+        num2=10;
+        System.out.println(num1 + " + "+ num2 + " = "+ (num1+num2));
+        System.out.println(num1 + " - "+ num2 + " = "+ (num1-num2));
+        System.out.println(num1 + " * "+ num2 + " = "+ (num1*num2));
+        System.out.println(num1 + " / "+ num2 + " = "+ ((float)num1/(float) num2));
+    }
+
+}
