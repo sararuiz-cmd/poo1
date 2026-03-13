7991599f303fc9f4a6d430fdd9d2eb71de749411
Sara Ruiz <svruiz@uamv.edu.ni>
2026-03-13 15:50:22 -0600

    JOptionPane

--- src/main/VariableReferencia.java
+++ src/main/VariableReferencia.java
@@ -1,9 +1,19 @@
 package main;
 
+import org.w3c.dom.xpath.XPathResult;
+
+import javax.swing.*;
 import java.util.Scanner;
 
 public class VariableReferencia {
     public static void main(String[] args) {
-        Scanner leer=new Scanner(System.in);
+        //Scanner leer=new Scanner(System.in);
+        //System.out.println("Dime tu nombre:");
+        //String nombre=leer.nextLine();
+        //System.out.println("Hola "+ nombre);
+        String nombre= JOptionPane.showInputDialog("Dime tu nombre: ");
+        String mensaje="Hola "+ nombre;
+
+        JOptionPane.showMessageDialog(null,mensaje+", bienvenido a la uam");
     }
 }
