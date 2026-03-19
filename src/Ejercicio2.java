7da0ab70a3192c9720bc1015cf2123205dcc5530
Sara Ruiz <svruiz@uamv.edu.ni>
2026-03-19 09:28:36 -0600

    Ejercicio 2 completo

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -2,15 +2,29 @@ import javax.swing.*;
 
 public class Ejercicio2 {
     static void main() {
-        int inicio=Integer.parseInt(JOptionPane.showInputDialog(null,"Dime un numero:"));
-        int fin=Integer.parseInt(JOptionPane.showInputDialog(null,"Dime otro numero:"));
-        int i=inicio;
-        String texto="";
-        while (i<fin){
-           if(i%2==0){
-               texto+=i+"\n";
-           }
+        int inicio = Integer.parseInt(JOptionPane.showInputDialog(null, "Dime un numero:"));
+        int fin = Integer.parseInt(JOptionPane.showInputDialog(null, "Dime otro numero:"));
+
+        int i = inicio;
+        String texto = "";
+
+        while (i <= fin) {
+            if (i % 2 == 0) {
+                String pasos="";
+                int factorial = 1;
+
+                for (int j = 1; j <= i; j++) {
+                    factorial *= j;
+                    if(j>1){
+                        pasos+=factorial+",";
+                    }
+                }
+
+                texto += i + ": " + pasos + "\n";
+            }
+            i++;
         }
+
         JOptionPane.showMessageDialog(null,texto);
     }
 }
